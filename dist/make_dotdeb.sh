3f04d7fbf7e9151cf663942efc86311188c2a4e6
Tyler Sommer <sommertm@gmail.com>
2018-12-10 13:22:44 -0700

    Extend circle config with version handling

--- dist/make_dotdeb.sh
+++ dist/make_dotdeb.sh
@@ -4,11 +4,14 @@ set -eo pipefail
 WORKING_DIR=$(dirname $(readlink -e "$BASH_SOURCE"))
 BUILD_ROOT=$(readlink -e "$WORKING_DIR/..")
 
-
-VERSION="2018.1.0"
+VERSION_BASE="2018.1.1"
+VERSION=${1:-"${VERSION_BASE}-snapshot"}
 
 export AGENT_VERSION="$VERSION"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_ROOT/build/linux_generic/rfs/lib"
+
+envsubst < "$WORKING_DIR"/debian/changelog.base > "$WORKING_DIR"/debian/changelog
+
 ln -sf "$WORKING_DIR/debian" "$BUILD_ROOT/debian"
 trap "rm -f \"$BUILD_ROOT\"/debian; trap - EXIT" EXIT
 
