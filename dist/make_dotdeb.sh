a967909567a0a7d7f432e8930c44bdddd8fb86a5
Tyler Sommer <sommertm@gmail.com>
2018-12-10 22:47:57 -0700

    Move /etc/default modifying to install.sh

--- dist/make_dotdeb.sh
+++ dist/make_dotdeb.sh
@@ -5,12 +5,17 @@ WORKING_DIR=$(dirname $(readlink -e "$BASH_SOURCE"))
 BUILD_ROOT=$(readlink -e "$WORKING_DIR/..")
 
 VERSION_BASE="2018.1.1"
+VERSION_STABILITY="unstable"
 VERSION=${1:-"${VERSION_BASE}-snapshot"}
 
 export AGENT_VERSION="$VERSION"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_ROOT/build/linux_generic/rfs/lib"
 
 envsubst < "$WORKING_DIR"/debian/changelog.base > "$WORKING_DIR"/debian/changelog
+
+if [[ ! -z "$1" ]]; then
+    sed -i -e 's/UNRELEASED/'"$VERSION_STABILITY"'/g' "$WORKING_DIR"/debian/changelog
+fi
 
 ln -sf "$WORKING_DIR/debian" "$BUILD_ROOT/debian"
 trap "rm -f \"$BUILD_ROOT\"/debian; trap - EXIT" EXIT
