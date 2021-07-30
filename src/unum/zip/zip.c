dae27fe1ec569b4aeb63960a93708f95b5895203
SethHager <85180782+SethHager@users.noreply.github.com>
2021-07-30 09:08:54 -0400

    zip: fix intializer for asus builds (#36)
    MIN-9256

--- src/unum/zip/zip.c
+++ src/unum/zip/zip.c
@@ -858,9 +858,11 @@ int LoadCentralDirectoryRecord(zip64_internal* pziinit)
 /************************************************************/
 extern zipFile ZEXPORT zipOpen3 (const void *pathname, int append, zipcharpc* globalcomment, zlib_filefunc64_32_def* pzlib_filefunc64_32_def)
 {
-    zip64_internal ziinit = {0};
+    zip64_internal ziinit;
     zip64_internal* zi;
     int err=ZIP_OK;
+
+    memset(&ziinit, 0, sizeof(zip64_internal));
 
     ziinit.z_filefunc.zseek32_file = NULL;
     ziinit.z_filefunc.ztell32_file = NULL;
