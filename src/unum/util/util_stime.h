28a1daf9425a3caa01c96fc8e9670a37ef51e9ae
Brett Mastbergen <brettm@minim.com>
2022-03-14 11:01:53 -0400

    util_stime: Remove XSTR() and STR() defines
    These came in with a commit intended to fix compile issues with
    2.31 and gcc 9.x, but I don't see how they are needed for that
    combination.  Since moving util_stime.h to util_common.h there are
    build errors with platforms that define these elsewhere.  Lets remove
    these from util_stime.h and see if anything breaks.  If a platform
    breaks because of the lack of these defines, we should include them
    in a platform specific header now that util_stime.h is common.
    
    SW-838

--- src/unum/util/util_stime.h
+++ src/unum/util/util_stime.h
@@ -5,9 +5,6 @@
 #ifndef _UTIL_STIME_H
 #define _UTIL_STIME_H
 
-#define XSTR(x) STR(x)
-#define STR(x) #x
-
 #if (__GLIBC__ > 2) || ((__GLIBC__ == 2) && __GLIBC_MINOR__ >= 31)
 int stime(const time_t *t);
 #endif
