2ddf97fd7b8097f0481edcbecde66728c494fa3f
Brett Mastbergen <brettm@minim.com>
2022-04-13 13:54:39 -0400

    zoom_7020_openwifi: Add handle_switch_config
    This function is called from switch_mode.sh when switching a
    device from mesh gateway to ap mode.
    
    SW-815

--- files/lede_generic/zoom_7020_openwifi/sbin/m_functions.sh
+++ files/lede_generic/zoom_7020_openwifi/sbin/m_functions.sh
@@ -1,3 +1,13 @@
+function handle_switch_config ()
+{
+	mode=$1
+	if [ "$mode" == "ap" ]; then
+		/sbin/uci set network.lan.ifname="eth0 eth1"
+	elif [ "$mode" == "gw" ]; then
+		/sbin/uci set network.lan.ifname="eth0"
+	fi
+	/sbin/uci commit network
+}
 
 function get_wan_if_name ()
 {
