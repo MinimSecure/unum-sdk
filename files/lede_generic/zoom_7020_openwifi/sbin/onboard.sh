a03a827f3001728672c73d3d01f82cf24fc56ce9
Brett Mastbergen <brettm@minim.com>
2022-04-28 09:58:32 -0400

    zoom_7020_openwifi: Create onboard sta on radio2
    The cloud creates the onboarding ap on radio2, so make sure the
    onboarding sta is created on radio2 as well.
    
    SW-1248

--- files/lede_generic/zoom_7020_openwifi/sbin/onboard.sh
+++ files/lede_generic/zoom_7020_openwifi/sbin/onboard.sh
@@ -11,9 +11,9 @@ function create_onboard_sta ()
 		/sbin/uci set network.wwan.ifname="sta0"
 		/sbin/uci set network.wwan.proto=dhcp
 
-		/sbin/uci set wireless.radio1.disabled=0
+		/sbin/uci set wireless.radio2.disabled=0
 		/sbin/uci set wireless.sta0="wifi-iface"
-		/sbin/uci set wireless.sta0.device='radio1'
+		/sbin/uci set wireless.sta0.device='radio2'
 		/sbin/uci set wireless.sta0.ifname='sta0'
 		/sbin/uci set wireless.sta0.network='wwan'
 		/sbin/uci set wireless.sta0.encryption='psk2/aes'
@@ -51,9 +51,9 @@ function create_onboard_ap ()
 	key=$3
 
 	if [ $create -eq 1 ]; then
-		/sbin/uci set wireless.radio1.disabled=0
+		/sbin/uci set wireless.radio2.disabled=0
 		/sbin/uci set wireless.onboard="wifi-iface"
-		/sbin/uci set wireless.onboard.device='radio1'
+		/sbin/uci set wireless.onboard.device='radio2'
 		/sbin/uci set wireless.onboard.network='lan'
 		/sbin/uci set wireless.onboard.encryption='psk2/aes'
 		/sbin/uci set wireless.onboard.mode='ap'
