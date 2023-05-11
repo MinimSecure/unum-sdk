65fefd5f7e4104322e48ddcf3dfb4f02d818edda
Venkata Rao Chimata <venkatch@gmail.com>
2023-05-11 19:42:03 +0530

    r14: Create a dummy wan interface to capture eth1 up / down events. (#177)
    Thsi WAN interface does nothing.
    
    Fixes SW-4029
    
    Signed-off-by: Venkata Chimata <venkatch@gmail.com>

--- files/lede_generic/minim_os/sbin/platform_boot.sh
+++ files/lede_generic/minim_os/sbin/platform_boot.sh
@@ -5,6 +5,7 @@ motorola,r14)
 	wanspeed=`cat /sys/class/net/eth1/speed`
 	if [ ! -f /tmp/fixed_phy.txt ] && [ 0$wanspeed -eq 100 ]; then
 		# We are fixing this once. Not any more
+		logger "Wan hack: Resetting WAN"
 		touch /tmp/fixed_phy.txt
 		ifconfig eth1 down; ifconfig eth1 up
 	fi
